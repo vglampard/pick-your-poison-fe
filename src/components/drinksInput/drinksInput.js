c94a6a29f9b357bdafb94ba1874739afddb741e1
BushraFatimaBF <vglampard@protonmail.com>
2023-02-07 12:27:49 +0000

    changed to normal input text fields for first pass

--- src/components/drinksInput/drinksInput.js
+++ src/components/drinksInput/drinksInput.js
@@ -1,130 +1,105 @@
 import Slider from "@mui/material/Slider";
 import Button from "@mui/material/Button";
 import { useState } from "react";
-import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { Paper } from "@mui/material";
 
 export default function DrinksInput() {
   const [headacheState, setHeadacheState] = useState(5);
   const [nauseaState, setNauseaState] = useState(5);
   const [fatigueState, setFatigueState] = useState(5);
-  const {
-    register,
-    handlSubmit,
-    formState: { errors },
-  } = useForm({});
+  const [wineState, setWineState] = useState(0);
+  const [beerState, setBeerState] = useState(0);
+  const [ciderState, setCiderState] = useState(0);
+  const [spiritState, setSpiritState] = useState(0);
+  const [alcopopState, setAlcopopState] = useState(0);
 
   function handleClick() {
-    console.log("final results:", nauseaState, headacheState, fatigueState);
+    console.log(
+      "final results:",
+      nauseaState,
+      headacheState,
+      fatigueState,
+      wineState,
+      beerState,
+      spiritState,
+      alcopopState,
+      ciderState
+    );
   }
-
-function handleSubmit(data, e){
-    e.preventDefault();
-    console.log("DATA:", data)
-}
   function changeNauseaValue(event, value) {
     setNauseaState(value);
     console.log(nauseaState);
   }
-
   function changeHeadacheValue(event, value) {
     setHeadacheState(value);
     console.log(headacheState);
   }
-
   function changeFatigueValue(event, value) {
     setFatigueState(value);
     console.log(fatigueState);
   }
 
-  function handleWineChange() {}
+  function handleWineChange(event) {
+    setWineState(event.target.value);
+    console.log(wineState);
+  }
+  function handleBeerChange(event) {
+    setWineState(event.target.value);
+  }
+  function handleCiderChange(event) {
+    setWineState(event.target.value);
+  }
+  function handleSpiritChange(event) {
+    setWineState(event.target.value);
+  }
+  function handleAlcopopChange(event) {
+    setWineState(event.target.value);
+  }
 
   return (
-    <div>
+    <Paper>
+      <label for="wine">Wine?</label>
+      <input
+        type="text"
+        id="wine"
+        name="wine"
+        onChange={handleWineChange}
+      ></input>
 
-<form
-			aria-label="Add new resource"
-			className="form"
-			onSubmit={handleSubmit}
-		>
-            <select
-						{...register("wine", { required: "Wine amount is required" })} aria-label = "wine-amount-dropdown"
-					>
-						<option value="">Wine</option>
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-						<option value="4">4</option>
-						<option value="5">5</option>
-						<option value="6">6</option>
-						<option value="7">7</option>
-						<option value="8">8</option>
-						<option value="9">9</option>
-						<option value="10">10</option>
-					</select>
-                    <select
-						{...register("cider", { required: "Cider amount is required" })} aria-label = "Cider-amount-dropdown"
-					>
-						<option value="">Cider</option>
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-						<option value="4">4</option>
-						<option value="5">5</option>
-						<option value="6">6</option>
-						<option value="7">7</option>
-						<option value="8">8</option>
-						<option value="9">9</option>
-						<option value="10">10</option>
-					</select>
-                    <select
-						{...register("spirit", { required: "Spirit amount is required" })} aria-label = "Spirit-amount-dropdown"
-					>
-						<option value="">Spirit</option>
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-						<option value="4">4</option>
-						<option value="5">5</option>
-						<option value="6">6</option>
-						<option value="7">7</option>
-						<option value="8">8</option>
-						<option value="9">9</option>
-						<option value="10">10</option>
-					</select>
-                    <select
-						{...register("beer", { required: "Beer amount is required" })} aria-label = "Beer-amount-dropdown"
-					>
-						<option value="">Beer</option>
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-						<option value="4">4</option>
-						<option value="5">5</option>
-						<option value="6">6</option>
-						<option value="7">7</option>
-						<option value="8">8</option>
-						<option value="9">9</option>
-						<option value="10">10</option>
-					</select>
-                    <select
-						{...register("alcopop", { required: "Alcopop amount is required" })} aria-label = "Alcopop-amount-dropdown"
-					>
-						<option value="">Alcopop</option>
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-						<option value="4">4</option>
-						<option value="5">5</option>
-						<option value="6">6</option>
-						<option value="7">7</option>
-						<option value="8">8</option>
-						<option value="9">9</option>
-						<option value="10">10</option>
-					</select>
+      <label for="beer">Beer?</label>
+      <input
+        type="text"
+        id="beer"
+        name="beer"
+        onChange={handleBeerChange}
+      ></input>
 
-                    <p>Nausea?</p>
+      <label for="cider">Cider?</label>
+      <input
+        type="text"
+        id="cider"
+        name="cider"
+        onChange={handleCiderChange}
+      ></input>
+
+      <label for="spirit">Spirits?</label>
+      <input
+        type="text"
+        id="spirit"
+        name="spirit"
+        onChange={handleSpiritChange}
+      ></input>
+
+      <label for="alcopop">Alcopop?</label>
+      <input
+        type="text"
+        id="alcopop"
+        name="alcopop"
+        onChange={handleAlcopopChange}
+      ></input>
+
+      <p>Nausea?</p>
       <Slider
         aria-label="nausea"
         className="slider"
@@ -136,7 +111,6 @@ function handleSubmit(data, e){
         marks
         min={0}
         max={10}
-        {...register("nausea", { required: "Nausea score is required" })}
       />
       <p>Headache?</p>
       <Slider
@@ -167,12 +141,6 @@ function handleSubmit(data, e){
       <Button variant="contained" onClick={handleClick}>
         Log drinks
       </Button>
-      <input id="submit-btn" type="submit" />
-</form>
-
-
-      
-    </div>
+    </Paper>
   );
 }
-
