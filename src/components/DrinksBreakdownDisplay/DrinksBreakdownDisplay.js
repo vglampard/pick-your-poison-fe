726c165fe3cb95f0d13604957a6bf8855e0833a6
BushraFatimaBF <vglampard@protonmail.com>
2023-02-09 23:58:53 +0000

    sketched out finction that conditionally renders icons in correct amounts for all drinks had (and not those NOT consumed)

--- src/components/DrinksBreakdownDisplay/DrinksBreakdownDisplay.js
+++ src/components/DrinksBreakdownDisplay/DrinksBreakdownDisplay.js
@@ -1,8 +1,13 @@
 import React from "react";
+import wine from "./wine.png"
+import cider from "./cider.png"
+import beer from "./beer.png"
+import spirit from "./spirit.png"
+import alcopop from "./alcopop.png"
+
 
 export default function DrinksBreakdownDisplay({ session }) {
-
- // Filter down the session object into just hte drinks values
+  // Filter down the session object into just hte drinks values
   const trimmedDrinks = (({ wine, beer, cider, spirit, alcopop }) => ({
     wine,
     beer,
@@ -11,22 +16,46 @@ export default function DrinksBreakdownDisplay({ session }) {
     alcopop,
   }))(session);
 
+  // function renderImages(number, img) {
+  //   let content = [];
+  //   for (let i = 0; i < number; i += 1) {
+  //     content.push(<img src={img} alt={img} aria-label={img} />);
+  //   }
+  //   return content;
+  // }
+
+  function renderAllIcons(drinks){
+    for (let drink of drinks){
+      let content = [];
+      for (let i = 0; i < drink.length; i += 1) {
+        content.push(<img src={drink} alt={drink} aria-label={drink} />);
+      }
+      return content;
+
+    }
+  }
+
   // Function that removes any null entries from the object (REFACTOR!)
   function trimToDrinks(trimmedDrinks) {
     let entries = Object.entries(trimmedDrinks);
     let finalTrim = [];
     for (let entry of entries) {
-        console.log("entry", entry)
+      console.log("entry", entry);
       if (entry[1] !== 0) {
         finalTrim.push(entry);
       }
     }
-    console.log("final trime object:", Object.fromEntries(finalTrim));
-    return Object.fromEntries(finalTrim);
+return finalTrim
+    // return Object.fromEntries(finalTrim);
   }
 
   const drinks = trimToDrinks(trimmedDrinks);
-  return <div>Here are your drinks:</div>;
-
-  
+  console.log("drinks final trimmed", drinks)
+  return <div>Here are your drinks:
+    {renderAllIcons(drinks)}
+  </div>;
 }
+
+
+// map over an array of arrays. 7
+// for each item in the array, render item[1] images of type item[0]
