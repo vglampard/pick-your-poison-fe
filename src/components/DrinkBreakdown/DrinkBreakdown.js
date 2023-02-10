e414e53337852695074f5df4c0cf82a6437155d9
BushraFatimaBF <vglampard@protonmail.com>
2023-02-10 10:42:58 +0000

    image TYPES are rendering correctly

--- src/components/DrinkBreakdown/DrinkBreakdown.js
+++ src/components/DrinkBreakdown/DrinkBreakdown.js
@@ -4,21 +4,27 @@ import cider from "./cider.png"
 import beer from "./beer.png"
 import spirit from "./spirit.png"
 import alcopop from "./alcopop.png"
-import styles from "./DrinkBreakdown"
 
 export default function DrinkBreakdown({drink, amount}) {
 
+const imageSources = {
+    cider: {cider},
+    beer: {beer},
+    wine: {wine},
+    spirit: {spirit},
+    alcopop: {alcopop}
+}
+
+console.log(imageSources[alcopop]);
 
     function renderAllIcons(drink, amount){
           let content = [];
           for (let i = 0; i < amount; i += 1) {
-            content.push(<img src={wine} alt={drink} aria-label={drink} style={{width: "20px"}}/>);
-            console.log("icon added")
+            content.push(<img src={imageSources[drink][drink]} alt={drink} aria-label={drink} style={{width: "20px"}}/>);
           }
           return content;
         }
       
-
   return (
     <div>    {renderAllIcons(drink, amount)}</div>
   )
